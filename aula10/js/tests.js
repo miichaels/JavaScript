55607e121a2bfe050594215ee091b1f691477986
miichaels <michael_araujo2012@hotmail.com>
2024-04-26 18:27:13 -0300

    alert / confirm / prompt

---
+++ aula10/js/tests.js
@@ -0,0 +1,25 @@
+alert('Hi, how are you?')
+
+// Metodos - alert / confirm / prompt
+//typeof (variavel...)
+//typeof num1
+
+confirm('Confirma que deseja apagar?')
+
+prompt('Digite seu nome.')
+
+const confirma = confirm('Realmente deseja apagar?');
+
+num1 = 1;
+
+console.log(typeof num1) //saber o tipo da variavel
+
+let num2 = prompt('Digite um numero');
+let num3 = prompt('Digite o outro numero');
+
+num2 = Number(num2);
+num3 = Number(num3);
+
+const resultado = num2 + num3;
+
+alert(`O resultado da soma dos numeros é de: ${resultado}`)

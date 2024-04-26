c27af044c2f8af5c6f194f17ef86e82e5cde6b33
miichaels <michael_araujo2012@hotmail.com>
2024-04-26 17:18:28 -0300

    operadores aritmeticos

---
+++ aula9/operadores.js
@@ -0,0 +1,25 @@
+/*
+Aritiméticos
++ Adição / Concatenação
+** Potenciação
+% Resto da divisão
+
+() | ** | * | /  | % | + | -
+
+Incremento = ++
+Decremento = --
+
+
+*/
+
+
+
+const num1 = 10;
+const num2 = 3;
+
+console.log(num1 % num2);
+
+let contador = 1;
+contador++ ;
+console.log(contador)
+
